fa3d60f9e40889352663f074a2831a441c5bbcd4
Levi <lte4@st.swps.edu.pl>
2025-12-17 19:54:11 +0100

    creat the program1

---
+++ program1.py
@@ -0,0 +1,13 @@
+print ("Calculate the total of number a and b")
+
+
+a = input ("Enter the first number")
+b = input ("Enter the second number")
+
+numbera = float(a)
+numberb = float(b)
+
+if ((numbera and numberb) = float)
+ Print (f'The total: a + b ={numbera + numberb}')
+else:
+ Print ("Invalid value")
