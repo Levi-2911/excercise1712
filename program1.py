6dfcebc09b2c3b13912264f39777788e6390eea6
Levi-2911 <tle4@st.swps.edu.pl>
2025-12-17 20:05:36 +0100

    add the /

--- program1.py
+++ program1.py
@@ -9,6 +9,7 @@ numberb = float(b)
 
 if ((numbera and numberb) = float)
  Print (f'The total: a + b ={numbera + numberb}')
- Print (f'The product: a x b ={numvera * numberb}')
+ Print (f'The product: a x b ={numbera * numberb}')
+ Print (f'The quotient: a / b ={numbera / numberb}')
 else:
  Print ("Invalid value")
